71f554a06339bf915fe08af1f03770b9f9183bf6
Yang Zhou <yangzhou@g.harvard.edu>
2023-08-18 07:41:09 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,8 @@ cd .. && make -j
 cd .. && Make -j
 cd advanced03-AF_XDP && make -j
 
-sudo ethtool -N ens1f1np1np1 rx-flow-hash udp4 fn
-sudo ethtool -N ens1f1np1np1 flow-type udp4 action 20
+sudo ethtool -N ens1f1np1 rx-flow-hash udp4 fn
+sudo ethtool -N ens1f1np1 flow-type udp4 action 20
 # In order to use zero-copy mode, must be queue id 10-20
 # Using `sudo ethtool -n ens1f1np1` to check existing rules
 # Using `sudo ethtool -N ens1f1np1 delete 1022` to delete any redundant rules, which impacts ens1f1np1 receiving packets in AF_XDP.
